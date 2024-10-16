9a0c0ee2b06044e0e895c68473556db7607d697f
Alexhr <alex.hernandez24kb@gmail.com>
2024-10-15 21:42:06 -0600

    go router

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme().getTheme(),
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
     );
   }
 }
---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        _CustomListTile(
+          title: 'State Provider', 
+          subTitile: 'Un estado simple', 
+          location: '/state-provider'
+        )
+      ],
+    );
+  }
+}
+
+
+class _CustomListTile extends StatelessWidget {
+
+  final String title;
+  final String subTitile;
+  final String location;
+
+  const _CustomListTile({
+    required this.title, 
+    required this.subTitile, 
+    required this.location
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(subTitile),
+      trailing: const Icon(Icons.arrow_forward_ios_rounded),
+      onTap:  () => context.push(location),
+    );
+  }
+}
