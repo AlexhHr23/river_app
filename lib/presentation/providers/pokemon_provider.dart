0a249cbd1763ad9e5025ca0c965dc253b7211095
Alexhr <alex.hernandez24kb@gmail.com>
2024-10-16 22:25:02 -0600

    CircularProgressIndicator

--- lib/presentation/providers/pokemon_provider.dart
+++ lib/presentation/providers/pokemon_provider.dart
@@ -1,7 +1,9 @@
 
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:river_app/config/config.dart';
 
 final pokemonProvider = FutureProvider<String>((ref) async {
-  return 'Picachu';
+  final name = PokemonService.getPokenonName(1);
+  return name;
 });
--- lib/presentation/screens/home/02-future-provider/pokemon_screen.dart
+++ lib/presentation/screens/home/02-future-provider/pokemon_screen.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:river_app/presentation/providers/pokemon_provider.dart';
 
-class PokemonScreen extends StatelessWidget {
+class PokemonScreen extends ConsumerWidget {
   const PokemonScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+
+    final pokemonAsync  = ref.watch(pokemonProvider);
+    
     return Scaffold(
       appBar: AppBar(
         title: Text('Future Provider'),
       ),
-      body: CircularProgressIndicator(),
+      body: Center(
+        child: pokemonAsync.when(
+          data: (value) => Text(value), 
+          error: (error, stackTrace) => Text('Error: $error'), 
+          loading: () => const CircularProgressIndicator()
+        )
+      ),
     );
   }
 }
